de96d55df50826904c4d461922c0c577870ba6d7
M.C.S.D. Jorge Apodaca <japodaca@bts.com.mx>
2017-11-15 17:11:27 -0800

    Final changes to the Azure.Storage.Files project

--- Azure.Storage.Files/Program.cs
+++ Azure.Storage.Files/Program.cs
@@ -2,8 +2,10 @@
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.File;
+using Microsoft.WindowsAzure.Storage.File.Protocol;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -31,15 +33,16 @@ namespace Azure.Storage.Files
 
 		static void Main(string[] args)
 		{
-			string path = "";
+			string path = "", pathRemote = "";
 			bool OPCION_EXISTE = true;
 
 			while (_opcion.KeyChar.ToString().Trim().ToUpper() != "X")
 			{
 				Console.Clear();
-				Console.WriteLine("(1) Listing files and directories from ROOT");
-				Console.WriteLine("(2) Listing files and directories from a path");
-				Console.WriteLine("(3) File upload");
+				Console.WriteLine("(1) Listing files and directories from a path");
+				Console.WriteLine("(2) Upload file");
+				Console.WriteLine("(3) Download file");
+				Console.WriteLine("(4) Delete file");
 				Console.WriteLine("(X) Exit\n\n");
 				Console.Write("Opcion: ");
 
@@ -48,12 +51,6 @@ namespace Azure.Storage.Files
 				switch (_opcion.KeyChar.ToString().Trim().ToUpper())
 				{
 					case "1":
-
-						GetFilesAndDirectories("/");
-
-						break;
-
-					case "2":
 
 						Console.Clear();
 						Console.Write("\n\nType '/' if you want the root directory, otherwise type the name of the hierarchy directories...\n\nPath: ");
@@ -64,17 +61,41 @@ namespace Azure.Storage.Files
 
 						break;
 
-					case "3":
+					case "2":
 
 						Console.Clear();
 						Console.WriteLine("\n\nType the path name and file you want to upload...");
+
+						Console.Write("\nLocal Path: ");
+						path = Console.ReadLine();
+						Console.Write("\nRemote Path: ");
+						pathRemote = Console.ReadLine();
+
+						if (String.IsNullOrEmpty(path)) FileUpload(); else FileUpload(path, pathRemote);
+
+						break;
+
+					case "3":
+
+						Console.Clear();
+						Console.WriteLine("\n\nType the path name and file you want to download...");
 						Console.WriteLine("\nPath: ");
 
 						path = Console.ReadLine();
 
-						if (String.IsNullOrEmpty(path)) FileUpload(); else FileUpload(path);
+						if (String.IsNullOrEmpty(path)) FileDownload(); else FileDownload(path);
 
-						Console.WriteLine("\n\nFile [{0}] uploaded...", path);
+						break;
+
+					case "4":
+
+						Console.Clear();
+						Console.WriteLine("\n\nType the path name and file you want to download...");
+						Console.WriteLine("\nPath: ");
+
+						path = Console.ReadLine();
+
+						if (String.IsNullOrEmpty(path)) FileDelete(); else FileDelete(path);
 
 						break;
 
@@ -95,17 +116,45 @@ namespace Azure.Storage.Files
 					OPCION_EXISTE = true;
 			}
 		}
-
-		static void FileUpload(string path = @"C:\Users\japodaca\OneDrive\MCSD\Pictures\badge.png")
+		
+		static void FileDelete(string path = @"dir01/dir02/image.png")
 		{
 			try
 			{
-				var file = _share.GetRootDirectoryReference().GetDirectoryReference("dir01").GetFileReference("image.png");
+				var file = _share.GetRootDirectoryReference().GetFileReference(path);
+				file.Delete();
+				Console.WriteLine("\n\nFile [{0}] deleted...", path);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+		}
 
+		static void FileDownload(string path = @"dir01/dir02/image.png")
+		{
+			try
+			{
+				var file = _share.GetRootDirectoryReference().GetFileReference(path);
+				file.DownloadToFile(@"C:\Temp\AzureFiles\image.png", FileMode.Create);
+				Console.WriteLine("\n\nFile [{0}] uploaded...", path);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+		}
+
+		static void FileUpload(string path = @"C:\Users\japodaca\OneDrive\MCSD\Pictures\badge.png", string remotePath = @"/badge.png")
+		{
+			try
+			{
 				using (var fileStream = System.IO.File.OpenRead(path))
 				{
+					var file = _share.GetRootDirectoryReference().GetFileReference(remotePath);
 					file.UploadFromStream(fileStream);
 				}
+				Console.WriteLine("\n\nFile [{0}] uploaded...", path);
 			}
 			catch (Exception ex)
 			{
@@ -122,21 +171,46 @@ namespace Azure.Storage.Files
 				_storageAccount = CloudStorageAccount.Parse(
 					CloudConfigurationManager.GetSetting("StorageConnection"));
 
-				SharedAccessAccountPolicy policy = new SharedAccessAccountPolicy()
-				{
-					Permissions = SharedAccessAccountPermissions.Read | SharedAccessAccountPermissions.Write | SharedAccessAccountPermissions.List,
-					Services = SharedAccessAccountServices.Blob | SharedAccessAccountServices.File,
-					ResourceTypes = SharedAccessAccountResourceTypes.Service,
-					SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24),
-					Protocols = SharedAccessProtocol.HttpsOnly
-				};
-				
-				StorageCredentials accountSAS = new StorageCredentials(_storageAccount.GetSharedAccessSignature(policy));
-
-				_storageAccount = new CloudStorageAccount(accountSAS, accountName: "justicia4gstore", endpointSuffix: null , useHttps: true);
-
 				_share = _storageAccount.CreateCloudFileClient().GetShareReference("documentos");
-				_share.CreateIfNotExists();
+
+				if (_share.Exists())
+				{
+					//Console.Clear();
+					// Check current usage stats for the share.
+					// Note that the ShareStats object is part of the protocol layer for the File service.
+					ShareStats stats = _share.GetStats();
+					//Console.WriteLine("Current share usage: {0} GB", stats.Usage.ToString());
+
+					// Specify the maximum size of the share, in GB.
+					// This line sets the quota to be 10 GB greater than the current usage of the share.
+					_share.Properties.Quota = 10 + stats.Usage;
+					_share.SetProperties();
+
+					// Now check the quota for the share. Call FetchAttributes() to populate the share's properties.
+					_share.FetchAttributes();
+					//Console.WriteLine("Current share quota: {0} GB", _share.Properties.Quota);
+
+					// Create a new shared access policy and define its constraints.
+					SharedAccessFilePolicy sharedPolicy = new SharedAccessFilePolicy()
+					{
+						SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24),
+						Permissions = SharedAccessFilePermissions.Read | SharedAccessFilePermissions.Write
+					};
+
+					// Get existing permissions for the share.
+					FileSharePermissions permissions = _share.GetPermissions();
+
+					if (!permissions.SharedAccessPolicies.ContainsKey("sampleSharePolicy"))
+					{
+						// Add the shared access policy to the share's policies. Note that each policy must have a unique name.
+						permissions.SharedAccessPolicies.Add("sampleSharePolicy", sharedPolicy);
+						_share.SetPermissions(permissions);
+					}
+
+					//Console.ReadKey();
+				}
+				else
+					_share.CreateIfNotExists();
 
 				#endregion
 			}
@@ -159,8 +233,8 @@ namespace Azure.Storage.Files
 
 				if (directory.Exists())
 				{
-					Console.WriteLine(String.Format("{0, -60} {1, -10} {2, -30}", "Directory or File", "Type", "Content Type"));
-					Console.WriteLine(String.Format("{0, -60} {1, -10} {2, -30}\n", (new String('=', 60)), (new String('=', 10)), (new String('=', 30))));
+					Console.WriteLine(String.Format("{0, -60} {1, -10} {2, -30} {3, 20}", "Directory or File", "Type", "Content Type", "Size"));
+					Console.WriteLine(String.Format("{0, -60} {1, -10} {2, -30} {3, -20}\n", (new String('=', 60)), (new String('=', 10)), (new String('=', 30)), (new String('=', 20))));
 					foreach (var item in directory.ListFilesAndDirectories())
 					{
 						if (item.GetType() == typeof(CloudFile))
@@ -168,10 +242,11 @@ namespace Azure.Storage.Files
 							((CloudFile)item).FetchAttributes();
 						}
 
-						Console.WriteLine(String.Format("{0, -60} {1, -10} {2, -30}",
+						Console.WriteLine(String.Format("{0, -60} {1, -10} {2, -30} {3, 20}",
 							item.GetType() == typeof(CloudFileDirectory) ? ((CloudFileDirectory)item).Name.Length > 60 ? ((CloudFileDirectory)item).Name.Substring(0, 30) : ((CloudFileDirectory)item).Name : ((CloudFile)item).Name.Length > 60 ? ((CloudFile)item).Name.Substring(0, 60) : ((CloudFile)item).Name,
 							item.GetType() == typeof(CloudFileDirectory) ? "Directory" : "File",
-							item.GetType() == typeof(CloudFile) ? ((CloudFile)item).Properties.ContentType.Length > 30 ? ((CloudFile)item).Properties.ContentType.Substring(0, 30) : ((CloudFile)item).Properties.ContentType : ""
+							item.GetType() == typeof(CloudFile) ? ((CloudFile)item).Properties.ContentType.Length > 30 ? ((CloudFile)item).Properties.ContentType.Substring(0, 30) : ((CloudFile)item).Properties.ContentType : "",
+							item.GetType() == typeof(CloudFile) ? String.Format("{0:N2} KB", (((CloudFile)item).Properties.Length / 1024)) : ""
 							));
 					}
 				}
